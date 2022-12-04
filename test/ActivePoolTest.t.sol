e90c4cf841b723d649d483aa8701939b63896fe0
Theo Le Fur <theo.lefur@epfl.ch>
2022-12-04 16:34:44 +0100

    initial commit

--- test/ActivePoolTest.t.sol
+++ test/ActivePoolTest.t.sol
@@ -4,24 +4,14 @@ pragma solidity <0.9.0;
 import "./UniswapTest.sol";
 
 contract ActivePoolTest is UniswapTest {
+
     function setUp() public {
         uniswapTest();
     }
 
-<<<<<<< HEAD
-    function testGetCollateralValue() public {
-        console.log(activePool.getCollateralValue());       
-    }
-}
-=======
-
-function setUp() public {
-    uniswapTest();
-}
-
 function testGetCollateralValue() public{
-    console.log(activePool.getCollateralValue());
+   // console.log(activePool.getCollateralValue());
     
 }
 }
->>>>>>> 13995ae35a7836b3729de97cc5fd515dbef31c72
+
