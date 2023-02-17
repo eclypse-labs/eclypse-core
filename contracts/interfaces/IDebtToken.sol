b30e4db73ad97ffd82a41f1b9e5e00ebde55daec
Darius Foodeei <darius.foodei@gmail.com>
2023-02-17 09:34:19 -0500

    add debttoken base

---
+++ contracts/interfaces/IDebtToken.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IGhoVariableDebtToken} from "gho-core/src/contracts/facilitators/aave/tokens/interfaces/IGhoVariableDebtToken.sol";
+
+interface IDebtToken is IGhoVariableDebtToken {}
