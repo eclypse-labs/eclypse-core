a877d8fd0ca042eb20cc46b09acf58c68ae01b5d
Darius Foodeei <darius.foodei@gmail.com>
2023-02-17 11:25:09 -0500

    add debttoken interface

--- contracts/DebtToken.sol
+++ contracts/DebtToken.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/IDebtToken.sol" as IDebtToken;
 
-contract DebtToken is IDebtToken {}
+contract DebtToken is IDebtToken {
+    event Mint(address indexed to, uint256 amount);
+    event Burn(address indexed from, uint256 amount);
+    event BorrowLimitChanged(uint256 newBorrowLimit);
+}
--- contracts/interfaces/IDebtToken.sol
+++ contracts/interfaces/IDebtToken.sol
@@ -3,4 +3,24 @@ pragma solidity ^0.8.0;
 
 import {IGhoVariableDebtToken} from "gho-core/src/contracts/facilitators/aave/tokens/interfaces/IGhoVariableDebtToken.sol";
 
-interface IDebtToken is IGhoVariableDebtToken {}
+interface IDebtToken is IGhoVariableDebtToken {
+    // Events
+    event Mint(address indexed to, uint256 amount);
+
+    event Burn(address indexed from, uint256 amount);
+
+    event BorrowLimitChanged(uint256 newBorrowLimit);
+
+    // Functions
+    function mint(address to, uint256 amount) external;
+
+    function burn(uint256 amount) external;
+
+    function getBorrowLimit() external view returns (uint256);
+
+    function setBorrowLimit(uint256 newBorrowLimit) external;
+
+    function decreaseBalanceFromInterest(address user, uint256 amount) external;
+
+    function getBalanceFromInterest(user) external view returns (uint256);
+}
