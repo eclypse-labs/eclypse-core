c98b505b8edbfa62355207423247911299e40c7b
Darius Foodeei <darius.foodei@gmail.com>
2023-04-07 20:26:17 +0200

    modified batchLiquidate with calldata - further mods needed

--- contracts/PositionsManager.sol
+++ contracts/PositionsManager.sol
@@ -536,12 +536,12 @@ contract PositionsManager is Ownable, IPositionsManager {
 
 	/**
 	 * @notice Liquidates multiple positions.
-	 * @dev Given that the callers have enough GHO to reimburse the positions' debt, the positions are liquidated, the GHO is burned and the NFTs are transfered to the callers.
-	 * @param _tokenIds The IDs of the positions to liquidate.
-	 * @param _amountRepays The amounts of GHO to repay to reimburse the debt of the positions.
+	 * @dev Given that the callers have enough stablecoin to reimburse the positions' debt, the positions are liquidated, the stablecoin is burned, and the NFTs are transferred to the callers.
+	 * @param _tokenIds The IDs of the positions to liquidate, using calldata to save gas.
+	 * @param _amountRepays The amounts of stablecoin to repay to reimburse the debt of the positions, using calldata to save gas.
 	 */
-	function batchliquidate(uint256[] memory _tokenIds, uint256[] memory _amountRepays) public override {
-		require(_tokenIds.length == _amountRepays.length);
+	function batchliquidate(uint256[] calldata _tokenIds, uint256[] calldata _amountRepays) public override nonReentrant {
+		require(_tokenIds.length == _amountRepays.length, "Input arrays must have the same length");
 		for (uint256 i = 0; i < _tokenIds.length; i++) {
 			liquidatePosition(_tokenIds[i], _amountRepays[i]);
 		}
